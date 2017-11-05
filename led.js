a945ff759ea936e8db15d2f06b9a10a2a20a7ed8
rafjordao <rfpj@cin.ufpe.br>
2017-11-05 11:21:25 -0300

    finalização dos websockets

--- led.js
+++ led.js
@@ -1,25 +1,64 @@
-var Gpio = require('onoff').Gpio,
-  buzzer = new Gpio(18, 'out'),
-  pir = new Gpio(28, 'in', 'both');
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED04 = new Gpio(4, 'out');
+
+LED04.writeSync(1);
 
 var WebSocketServer = require('ws').Server;
-wss = new WebSocketServer({port: 8081, path: '/moviment_sensor'});
+wss = new WebSocketServer({port: 8082, path: '/shut_down'});
+var funcao = true;
+var ativa = true;
 
- 
-	pir.watch(function(err, value) {
-  		if (err) exit();
-  		buzzer.writeSync(value);
-  		//console.log('Intruder detected'+ new Date());
-  		if(value == 1){
-			console.log("tem gente"+ new Date());
-			ws.send("sensor ativado");
-   		}
-	}); 
-console.log('Pi Bot deployed successfully!');
-console.log('Guarding the Magic pencil...');
- 
-function exit() {
-  
-  pir.unexport();
-  process.exit();
-}
+wss.on('connection', function(ws) {
+
+	LED04.writeSync(1);
+
+
+
+	function unexportOnClose() { //function to run when exiting program
+		LED04.writeSync(0);
+	};
+
+console.log("led ok");
+
+ws.on('message', function(message) {
+
+	if(message.includes("Tomada") && message.includes("Ativa")){
+		ativa = true;
+	}else if (message.includes("Tomada") && message.includes("Desativar")){
+		ativa = false;
+	}	
+	
+	if(message.includes("Função") && message.includes("Ativar")){
+	  funcao = true;
+	}else if (message.includes("Função") && message.includes("Desativar")){
+	  funcao = false;	
+	};
+	
+	console.log("tomada :" +ativa);
+	console.log("funcao :" +funcao);
+
+	if(ativa){
+		if(funcao){
+			console.log(message);
+	
+			if(message.includes("liberada")){
+			 	LED04.writeSync(1);
+			}else{
+				LED04.writeSync(0);
+			};
+		}else{
+			LED04.writeSync(1);
+		};
+	}else{
+		LED04.writeSync(0);	
+	}
+	
+
+	
+	
+});
+
+
+
+});
+
